f0238c40fba7da7e3d46673533cd359977e3b215
isuke <isuke770@gmail.com>
2017-10-29 23:59:03 +0900

    Add function of filter comments

--- lib/poe_filter_generator/filter.rb
+++ lib/poe_filter_generator/filter.rb
@@ -3,6 +3,7 @@ class PoeFilterGenerator::Filter
 
   def initialize(name, export_path, aliases)
     @name = name
+    @comments = []
     @export_path = export_path
     @aliases = aliases
     @groups = []
@@ -10,16 +11,28 @@ class PoeFilterGenerator::Filter
 
   def generate
     File.open(File.join(@export_path, "#{@name}.filter"), 'w') do |file|
-      file.puts '#' * 80
-      file.puts '#'
       file.puts "# generate by poe_filter_generator ver #{PoeFilterGenerator::VERSION}"
-      file.puts '#'
-      file.puts '#' * 80
+      file.puts "# https://github.com/isuke/poe_filter_generator"
+      file.puts ''
+      if @comments.any?
+        file.puts '#' * 80
+        file.puts '#'
+        @comments.each do |comment|
+          file.puts "# #{comment}"
+        end
+        file.puts '#'
+        file.puts '#' * 80
+        file.puts ''
+      end
 
       @groups.each do |group|
         group.generate(file)
       end
     end
+  end
+
+  def comment(str)
+    @comments << str
   end
 
   def group(name)
